d92631c8973addc02be906f1a70348d28f4c6424
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-10-07 13:20:48 -0700

    reviews endpoint installed and passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -44,5 +44,33 @@ describe('app routes', () => {
 
       expect(data.body).toEqual(expect.arrayContaining(expectation));
     });
+
+    // Reviews
+    test('returns reviews data', async() => {
+
+      const expectation = [
+        {
+          'name': 'Pike Place Chowder',
+          'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/ijju-wYoRAxWjHPTCxyQGQ/o.jpg',
+          'price': '$$   ',
+          'rating': '4.5',
+          'url': 'https://www.yelp.com/biz/pike-place-chowder-seattle?adjust_creative=uK0rfzqjBmWNj6-d3ujNVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=uK0rfzqjBmWNj6-d3ujNVA'
+        },
+        {
+          'name': 'Umi Sake House',
+          'image_url': 'https://s3-media3.fl.yelpcdn.com/bphoto/c-XwgpadB530bjPUAL7oFw/o.jpg',
+          'price': '$$   ',
+          'rating': '4.0',
+          'url': 'https://www.yelp.com/biz/umi-sake-house-seattle?adjust_creative=uK0rfzqjBmWNj6-d3ujNVA&utm_campaign=yelp_api_v3&utm_medium=api_v3_business_search&utm_source=uK0rfzqjBmWNj6-d3ujNVA'
+        }       
+      ];     
+
+      const data = await fakeRequest(app)
+        .get('/reviews')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expect.arrayContaining(expectation));
+    });
   });
 });

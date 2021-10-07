131a42054f9683080eeee1b3a842efab8fafa01b
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-10-07 13:23:25 -0700

    trails

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -72,5 +72,43 @@ describe('app routes', () => {
 
       expect(data.body).toEqual(expect.arrayContaining(expectation));
     });
+
+    // Trails
+    test('returns trails data', async() => {
+
+      const expectation = [
+        {
+          'name': 'Rattlesnake Ledge',
+          'location': 'Riverbend, Washington',
+          'length': '4.3',
+          'stars': '4.4',
+          'star_votes': '84',
+          'summary': 'An extremely popular out-and-back hike to the viewpoint on Rattlesnake Ledge.',
+          'trail_url': 'https://www.hikingproject.com/trail/7021679/rattlesnake-ledge',
+          'conditions': 'Dry: The trail is clearly marked and well maintained.',
+          'condition_date': '2018-07-21',
+          'condition_time': '0:00:00 '
+        },
+        {
+          'name': 'Mt. Si',
+          'location': 'Tanner, Washington',
+          'length': '6.6',
+          'stars': '4.4',
+          'star_votes': '72',
+          'summary': 'A steep, well-maintained trail takes you atop Mt. Si with outrageous views of Puget Sound.',
+          'trail_url': 'https://www.hikingproject.com/trail/7001016/mt-si',
+          'conditions': 'Dry',
+          'condition_date': '2018-07-22',
+          'condition_time': '0:17:22 '
+        }
+      ];           
+
+      const data = await fakeRequest(app)
+        .get('/trails')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(data.body).toEqual(expect.arrayContaining(expectation));
+    });
   });
 });
